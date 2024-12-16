67ff6fe1fff2d8b0e8eb3065cfab4eb09bf42194
Julien Greuter <julien_g@LAPTOP-N3R34PGU>
2024-12-16 13:30:15 +0100

    correction de l'oublie de l'attribut masse pour Particules

--- sous_projet_modelisation2D/sous_projet_0_particules_ponctuelles_LJ/particules.h
+++ sous_projet_modelisation2D/sous_projet_0_particules_ponctuelles_LJ/particules.h
@@ -14,9 +14,10 @@ public:
     std::vector<Vec2> positions;  // Positions des particules
     std::vector<Vec2> vitesses;   // Vitesses des particules
     double taille;                // Taille des particules (initialisée à 0 pour des particules ponctuelles)
+    double masse;                 // Masse des particules
 
     // Constructeur
-    Particules(int N, double E_0, double d, double charge = 0.0, double taille = 0.0);
+    Particules(int N, double E_0, double d, double charge = 0.0, double taille = 0.0, double masse);
 
     // Méthode pour initialiser les vitesses en fonction de la température T
     void initialiserVitesses(double T);
